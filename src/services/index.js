3882ffbe986ba91c3e9b7c7400ac9fe9361738e2
Pavel Panin <paa.panin@mail.ru>
2022-04-07 13:38:09 +0300

    feature/fix_api_url

--- src/services/index.js
+++ src/services/index.js
@@ -3,7 +3,7 @@ import { API } from "./api";
 import { Heros } from "./heros";
 
 export const ApiServices = new API(
-  "http://hp-api.herokuapp.com/api/characters/students"
+  "https://hp-api.herokuapp.com/api/characters/students"
 );
 
 const HerosServices = new Heros(ApiServices);
